5c84be4fe7344c3a1da6f992169f5c2db5b16d96
Kpate372 <43353643+Kpate372@users.noreply.github.com>
2021-03-05 22:20:37 -0500

    Add update functionality for user

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use((req,res,next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-with, Content-Type, Accept");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
   next();
 });
 
@@ -36,8 +36,21 @@ app.post("/api/posts", (req, res, next) => {
     });
   });
 });
-//8k2KdpemBzAvmSJ
-//99.234.119.190
+
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Update successful!" });
+  });
+});
+
+
 app.get("/api/posts", (req, res, next) => {
   Post.find().then(documents => {
     res.status(200).json({
